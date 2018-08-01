a5c13da725f9a594bf11655e16a2fbfe2967bbc3
Marek Ventur <marekventur@gmail.com>
2018-08-01 21:43:36 +0100

    Commit for testing purposes. Please don't delete

--- launcher/main.py
+++ launcher/main.py
@@ -3,23 +3,13 @@
 ___name___         = "Launcher"
 ___license___      = "MIT"
 ___categories___   = ["System"]
+___dependencies___ = ["shared/test/download.txt"]
 ___launchable___   = False
 ___bootstrapped___ = True
 
 import ugfx
 
 ugfx.clear()
-apps = range(1, 10)
-
-cols = 4
-
-
-for i, p in enumerate(apps):
-    logical_x = i % cols
-    logical_y = i // cols
-    x = logical_x * width
-
-
 
 
 print("launcher")
