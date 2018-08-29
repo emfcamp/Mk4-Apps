642b4a0d16b2f3d9659b822cc1517e011e5ff0b2
Alistair MacDonald <alistairuk@users.noreply.github.com>
2018-08-29 19:58:04 +0100

    Added SMS sending

--- sms/main.py
+++ sms/main.py
@@ -18,14 +18,30 @@ ugfx.clear()
 menuset = []
 messages = sim800.listsms(4)
 
+def send_message():
+    number = ""
+    message = ""
+    while True:
+        num = prompt_text("Number to message:", init_text=number)
+        if num is None:
+            return
+        message = prompt_text("Message:", init_text=message)
+        if message is not None:
+            if sim800.sendsms(number, message):
+                return
+
 for message in messages:
     splitmessage = message.split(",")
     menuset.insert(0, { "title" : splitmessage[5] + " " + splitmessage[4] + " from " + splitmessage[2], "index" : splitmessage[0] })
 
+menuset.insert(0, { "title" : "Send message...", "index" : -1 })
+
 while True:
-	selection = prompt_option(menuset, text="Select message", select_text="Read", none_text="Exit")
-	if (selection):
-		message = sim800.readsms(selection["index"])
-		notice(message, title=selection["title"])
-	else:
-		break
+    selection = prompt_option(menuset, text="Select message", select_text="Read", none_text="Exit")
+    if (selection["index"]==-1):
+        send_message()
+    elif (selection):
+        message = sim800.readsms(selection["index"])
+        notice(message, title=selection["title"])
+    else:
+        break
