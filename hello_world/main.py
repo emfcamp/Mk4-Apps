ea116a72b49922aa889fccae6de3fb826c14b8c4
Benjamin Boortz <benjamin.boortz@mailbox.org>
2018-09-02 10:41:52 +0100

    closing implemented

--- hello_world/main.py
+++ hello_world/main.py
@@ -5,11 +5,22 @@ ___license___      = "MIT"
 ___dependencies___ = ["sleep", "app"]
 ___categories___   = ["EMF"]
 
-import ugfx, os, time, sleep, app
+import ugfx, sleep, app
 
 
 # initialize screen
 ugfx.init()
 ugfx.clear()
 
+# show text
 ugfx.text(5, 5, "Hello World!", ugfx.BLACK)
+
+
+# waiting until a button has been pressed
+while (not Buttons.is_pressed(Buttons.BTN_A)) and (not Buttons.is_pressed(Buttons.BTN_B)) and (not Buttons.is_pressed(Buttons.BTN_Menu)):
+    sleep.wfi()
+
+
+# closing
+ugfx.clear()
+app.restart_to_default()
