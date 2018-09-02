7c27c018cfb31b406fa068056f843eb2a4f4491a
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2018-09-02 13:20:43 +0100

    update test

--- serendipity/main.py
+++ serendipity/main.py
@@ -2,31 +2,14 @@
 
 ___name___         = "serendipity"
 ___license___      = "MIT"
-___dependencies___ = ["sleep", "app", "ugfx_helper"]
+___dependencies___ = ["sleep", "app"]
 ___categories___   = ["EMF", "Other"]
 
-import ugfx_helper, ugfx, os, time, sleep, app
-from tilda import Buttons
+import ugfx, os, time, sleep, app
+
 
 # initialize screen
 ugfx.init()
 ugfx.clear()
 
-#ugfx.text(5, 5, "[work in progress]", ugfx.BLACK)
-
-def show_screen():
-  ugfx.display_image( 0, 0, "serendipity/main.png" )
-
-  
-show_screen()
-  
-while True:
-  
-  sleep.wfi()
-
-  if Buttons.is_pressed( Buttons.BTN_Menu ) or \
-    Buttons.is_pressed( Buttons.BTN_B ) or \
-    Buttons.is_pressed( Buttons.JOY_Center):
-    break
-  
-ugfx.clear()
+ugfx.text(5, 5, "[test]", ugfx.BLACK)
