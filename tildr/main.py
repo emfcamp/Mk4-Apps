116613f4240b4331c4bf9e1a8682bbe6eb107736
Daniel Saul <daniel@dansaul.co.uk>
2018-09-02 13:28:54 +0100

    Massive refactor: seperate files, screen state, dialog back, edit profile, local images

--- tildr/main.py
+++ tildr/main.py
@@ -6,8 +6,12 @@ ___dependencies___ = ["wifi", "http", "ugfx_helper", "sleep", "dialogs", "sim800
 ___categories___   = ["Other"]
 ___bootstrapped___ = True
 
-import app, buttons, ugfx, ugfx_helper, http, dialogs, sim800, database, ujson
+import app, buttons, ugfx, ugfx_helper, sleep, http, dialogs, sim800, database, ujson
 from tilda import Buttons
+
+from tildr.profile import get_profile
+from tildr.shared import clear
+from tildr import splash, profile, person, nomore
 
 api_url = "http://emf2018.us-east-2.elasticbeanstalk.com"
 
@@ -19,173 +23,63 @@ style.set_enabled([ugfx.WHITE, ugfx.WHITE, ugfx.html_color(0x888888), ugfx.html_
 style.set_background(ugfx.html_color(0x000000))
 ugfx.set_default_style(style)
 
-def render_splash_screen():
-    ugfx.clear(ugfx.html_color(0x000000))
-    try:
-        logo = http.get("https://i.imgur.com/0TjxEPs.png").raise_for_status().content
-        ugfx.display_image(
-            int((ugfx.width() - 164)/2),
-            20,
-            bytearray(logo))
-    except:
-        pass
+def error_screen(state):
+    ugfx.text(5, 100, "Error: try again later :(", ugfx.WHITE)
 
-    ugfx.text(160, 100, "TILDR", ugfx.WHITE)
-    ugfx.text(0, 270, "Find your match @emfcamp ;)", ugfx.WHITE)
-    ugfx.text(45, 300, "Press A to begin", ugfx.WHITE)
+def error_actions(state):
+    if buttons.is_triggered(Buttons.BTN_A):
+        state['next'] = "SPLASH"
 
+screens = {
+    'SPLASH': {'render': splash.screen, 'actions': splash.actions},
+    'PROFILE': {'render': profile.screen, 'actions': profile.actions},
+    'ERROR': {'render': error_screen, 'actions': error_actions},
+    'NEXT_PERSON': {'render': person.screen, 'actions': person.actions},
+    'NO_MORE': {'render': nomore.screen, 'actions': nomore.actions}
+}
 
-def run_splash_screen():
-    while True:
-        if buttons.is_triggered(Buttons.BTN_Menu):
-            return False
-        if buttons.is_triggered(Buttons.BTN_A):
-            return True
-
-def create_profile(my_profile):
-    ugfx.clear(ugfx.html_color(0x000000))
-
-    name, age = "", ""
-    while name == "":
-        name = dialogs.prompt_text("What's your name?")
-    while age == "":
-        age = dialogs.prompt_text("What's your age?")
-    tag_line = dialogs.prompt_text("Tell us your tagline:")
-    looking_for = dialogs.prompt_text("And what you're looking for:")
-    contact = dialogs.prompt_text("And your twitter username?")
-    imei = sim800.imei()
-
-    top_left_logo()
-    ugfx.text(5, 100, "Working...", ugfx.BLACK)
-
-    profile = {
-        'unique_identifier': imei,
-        'username': name,
-        'age': age,
-        'tag_line': tag_line,
-        'looking_for': looking_for,
-        'contact': contact
-    }
-
-    profile_json = ujson.dumps(profile)
-
-    try:
-        http.post(api_url+'/create_user', json=profile).raise_for_status().close()
-    except:
-        ugfx.clear()
-        ugfx.text(5, 100, "Error. Try again later. :(", ugfx.BLACK)
-        return False
-
-    database.set("tildr_profile", profile_json)
-
-    return True
+state = {
+    'api': api_url,
+    'running': True,
+    'screen': None,
+    'next': "SPLASH",
+    'ui': [],
+    'profile': None
+}
 
 
-def main_screen(my_profile):
-    while True:
-        next_person(my_profile)
-        while True:
-            if buttons.is_triggered(Buttons.BTN_Menu):
-                return False
-            if buttons.is_triggered(Buttons.BTN_B) or buttons.is_triggered(Buttons.JOY_Right):
-                break
-            # if buttons.is_triggered(Buttons.BTN_A) or buttons.is_triggered(Buttons.JOY_Left):
-            #     create_profile(my_profile)
-            #     break
+def destroy(state):
+    for item in state['ui']:
+        try:
+            item.hide()
+        except:
+            pass
+        item.destroy()
+
+    state['ui'] = []
 
 
-def next_person(my_profile):
-    ugfx.clear(ugfx.html_color(0x000000))
-    ugfx.text(5, 100, "Loading...", ugfx.WHITE)
-    try:
-        resp = http.get(api_url+'/get_user/'+my_profile['unique_identifier']).json()
-    except:
-        ugfx.clear()
-        ugfx.text(5, 100, "Error. Try again later. :(", ugfx.BLACK)
-        return
+state['profile'] = get_profile()
 
-    if resp['success']:
-        display_person(resp['value'])
-    else:
-        no_more(my_profile)
+while state['running']:
 
+    # Move to next screen
+    if state['next']:
+        destroy(state)
+        nxt = state['next']
+        state['screen'] = nxt
+        state['next'] = None
 
-def display_person(person):
-    top_left_logo()
+        clear()
+        screens[nxt]['render'](state)
 
-    ugfx.set_default_font(ugfx.FONT_TITLE)
-    ugfx.Label(5, 90, 230, 40, person["username"], justification=ugfx.Label.LEFTTOP)
-    ugfx.set_default_font(ugfx.FONT_SMALL)
-    ugfx.text(200, 92, person["age"], ugfx.WHITE)
+    sleep.wfi()
 
-    ugfx.Label(5, 120, 230, 60, person["tag_line"])
+    s = state['screen']
+    screens[s]['actions'](state)
 
-    ugfx.Label(5, 200, 230, 40, person["looking_for"])
-    ugfx.text(5, 190, "Looking for...", ugfx.RED)
+    if buttons.is_triggered(Buttons.BTN_Menu):
+        state['running'] = False
 
-    ugfx.text(5, 245, person["contact"], ugfx.BLUE)
-
-    # ugfx.Button(0, 280, 100, 40, "< Edit profile", parent=None, shape=ugfx.Button.RECT, style=None)
-    ugfx.Button(160, 280, 100, 40, "Swipe >", parent=None, shape=ugfx.Button.RECT, style=None)
-
-
-def no_more(my_profile):
-    top_left_logo()
-
-    ugfx.set_default_font(ugfx.FONT_TITLE)
-    ugfx.Label(5, 90, 230, 50, "You've swiped everybody!", justification=ugfx.Label.CENTERTOP)
-    ugfx.set_default_font(ugfx.FONT_SMALL)
-    ugfx.Label(5, 160, 230, 20, "Soz "+my_profile["username"], justification=ugfx.Label.CENTERTOP)
-    ugfx.Label(5, 180, 230, 20, "Come back later ;)", justification=ugfx.Label.CENTERTOP)
-
-    # ugfx.Button(0, 280, 100, 40, "< Edit profile", parent=None, shape=ugfx.Button.RECT, style=None)
-    ugfx.Button(160, 280, 100, 40, "Try again >", parent=None, shape=ugfx.Button.RECT, style=None)
-
-
-def get_profile():
-    profile_json = database.get("tildr_profile")
-    if profile_json is None:
-        return None
-
-    profile = ujson.loads(profile_json)
-    return profile
-
-
-def top_left_logo():
-    ugfx.clear(ugfx.html_color(0x000000))
-    try:
-        logo = http.get("https://i.imgur.com/5HXmXBU.png").raise_for_status().content
-        ugfx.display_image(0, 5, bytearray(logo))
-    except:
-        pass
-
-
-def quit_loop():
-    while True:
-        if buttons.is_triggered(Buttons.BTN_Menu):
-            return False
-
-
-while True:
-
-    profile = get_profile()
-
-    if profile is None:
-        render_splash_screen()
-        if not run_splash_screen():
-            break
-        profile = {
-            'username': "",
-            'age': "",
-            'tag_line': "",
-            'looking_for': "",
-            'contact': ""
-        }
-        if not create_profile(profile):
-            continue
-
-    main_screen(profile)
-    if not quit_loop():
-        break
 
 app.restart_to_default()
