24c8935cd03bb25657f52660bc968a89b6e6deb6
Illlin <34010016+Illlin@users.noreply.github.com>
2018-09-05 19:41:57 +0100

    Fixed dependency error

--- DevRant/main.py
+++ DevRant/main.py
@@ -2,7 +2,7 @@
 """
 ___name___         = "DevRant"
 ___license___      = "MIT"
-___dependencies___ = ["app", "wifi", "http", "homescreen", "utime", "ugfx_helper", "dialogs"]
+___dependencies___ = ["app", "wifi", "http", "homescreen", "ugfx_helper", "dialogs"]
 ___categories___   = ["Other"]
 ___launchable___   = True
 
